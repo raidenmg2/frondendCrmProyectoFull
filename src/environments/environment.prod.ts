655f58e88c0ab0ef3e956b97724c07c881c908d7
Luis Carlos Villalobos Romero <raidenmg2@gmail.com>
2024-04-17 13:39:25 -0500

    actualizando frond para despliegue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment ={
     // en este espacio se declaran todas las url del proyecto 
     production: true,
-    base_url: 'http://localhost:5000/api/v1',
+    base_url: 'https://backendcrmproyect.onrender.com/api/v1',
 };
